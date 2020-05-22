28cd327a7f01d7b2794ac6bd9ba3a603f0152177
lucaszaranza <lucaszaranza@gmail.com>
2020-05-22 12:19:21 -0300

    Updates

---
+++ precache-manifest.4828750b17c30990da33512cf6398724.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6d10b61f6e9d191a5ee39726cdda91",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "624404ffda4422770c24",
+    "url": "/robots-rolodex/static/css/main.afab3340.chunk.css"
+  },
+  {
+    "revision": "95b023db6fa5f95ba50f",
+    "url": "/robots-rolodex/static/js/2.9523cfc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots-rolodex/static/js/2.9523cfc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "624404ffda4422770c24",
+    "url": "/robots-rolodex/static/js/main.63ae3bd2.chunk.js"
+  },
+  {
+    "revision": "bc675e27fa50baefeb3d",
+    "url": "/robots-rolodex/static/js/runtime-main.88d46e6d.js"
+  }
+]);
